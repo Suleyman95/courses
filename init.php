da6f36208c8bf03687bb04ea59d4839e67b58009
Suleyman <youmsv@gmail.com>
2017-11-21 13:14:45 +0300

    Добавляет init.php

---
+++ init.php
@@ -0,0 +1,16 @@
+<?php
+
+$link = mysqli_connect("localhost", "root", "0202", "base");
+if(!$link) { 
+  // если была ошибка выводим ее описание
+  echo mysqli_connect_error($link);
+}
+else 
+{
+  // устанавливаем кодировку
+  mysqli_set_charset($link, "utf8");
+}
+
+session_start();
+
+?>
